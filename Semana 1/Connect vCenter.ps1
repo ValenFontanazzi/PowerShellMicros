df5f95da616801a40dfaf513125c00082cc1af5e
Valentino <valenfontanazzi@gmail.com>
2022-05-31 11:37:54 -0300

    semana1y2

---
+++ Semana 1/Connect vCenter.ps1
@@ -0,0 +1,5 @@
+﻿$user = 'vfontanazzi'
+$pass = 'Password123$'
+$playground = '192.168.12.60'
+
+Connect-VIServer -Server $playground -User $user -Password  $pass
---
+++ Semana 2/Snapshots.ps1
@@ -0,0 +1,20 @@
+﻿$user = "vfontanazzi"
+$pass = "Password123$"
+$playground = '192.168.12.60'
+
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm:$false
+Connect-VIServer -Server $playground -User $user -Password  $pass
+
+$todayDate = Get-Date
+
+$WeekBefore = (Get-Date).AddDays(-7)
+
+$snapshots = Get-VM | Get-Snapshot | Where-Object -Property Created -lt $WeekBefore
+
+$snapshots | Select-Object VM, created | Export-Csv -Path C:\Data.csv
+
+Import-Csv -Path C:\ Data.csv
+
+
+$datastores = Get-Datastore | Get-VM | select name,usedspacegb
