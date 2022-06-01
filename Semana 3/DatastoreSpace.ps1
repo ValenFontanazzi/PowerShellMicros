75b61ba2a7d62c6ad55af603f1110d5ffe8ba7c9
Valentino <valenfontanazzi@gmail.com>
2022-06-01 14:22:37 -0300

    upload diagrams, new semana3

---
+++ Semana 3/DatastoreSpace.ps1
@@ -0,0 +1,13 @@
+﻿$user = "vfontanazzi"
+$pass = "Password123$"
+$playground = '192.168.12.60'
+
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm:$false
+Connect-VIServer -Server $playground -User $user -Password  $pass
+
+$datastores = Get-Datastore
+ 
+$datastores | Select-Object name,Datacenter,CapacityGB,FreeSpaceGB | Export-Csv -Path C:\DatastoresSpace.csv
+
+Import-Csv -Path C:\DatastoresSpace.csv
