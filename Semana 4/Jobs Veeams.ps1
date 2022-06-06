9724078055b7a48c7cfda932df1c0eae52d4df68
Valentino <valenfontanazzi@gmail.com>
2022-06-06 13:21:51 -0300

    Semana 4

---
+++ Semana 4/Jobs Veeams.ps1
@@ -0,0 +1,13 @@
+﻿$jobs = Get-VBRJob 
+
+$table = New-Object System.Data.Datatable
+[void]$table.Columns.Add("Job Name","Last Result","Descripcion")
+
+foreach ($job in $jobs)
+{
+    $name = $job.Name
+    $lastresult = $job.GetLastResult()
+    $desc = $job.Description
+    [void]$table.Rows.Add($name,$lastresult,$desc)
+}
+$table | export-csv C:\DataTableJobs.csv 
