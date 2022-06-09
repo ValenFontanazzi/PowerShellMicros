0ca2aff27b5f93b64ad601e5a10836f5463ef2a8
Valentino Fontanazzi <36343931+ValenFontanazzi@users.noreply.github.com>
2022-06-09 13:34:49 -0300

    semana 6 finished

--- Semana 6/IdleVMs.ps1
+++ Semana 6/IdleVMs.ps1
@@ -1,48 +1,33 @@
-﻿$user = "vfontanazzi"
+﻿$user ="vfontanazzi"
 $pass = "Password123$"
 $playground = '192.168.12.60'
 
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
-Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm:$false
-Connect-VIServer -Server $playground -User $user -Password  $pass
+Set-PowerCLIConfiguration - InvalidCertificateAction Ignore -Confirm: $false
+Set-Power CLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm: $false
+Connect-VIServer -Server $playground -User $user -Password $pass
 
-$table = New-Object System.Data.Datatable
+$table = New-Object System.Data. Datatable
 
-$table.Columns.Add("VM","Assigned Cpu","Used Cpu","Assigned Memory","Used Memory")
-
-$vms = Get-VM | Select-Object -Property Name
-
-foreach ($vm in $vms) { 
-    $vmdata = get-view –viewtype VirtualMachine –filter @{“Name”=$vm.Name} 
-    $vmucpu = $vmdata.Summary.QuickStats.OverallCpuUsage
-    if($vmucpu -lt 100){
-        
-        $vmacpu = $vmdata.Summary.Runtime.MaxCpuUsage
-        $vmumem = $vmdata.Summary.QuickStats.GuestMemoryUsage
-        $vmamem = $vmdata.Summary.Runtime.MaxMemoryUsage
-
-        $vm.name
-        $vmacpu
-        $vmucpu
-        $vmamem
-        $vmumem
+[void] $table.Columns.Add("VM")
+[void] $table.columns.Add("Assigned")
+[void] $table.Columns.Add("Used CPU")
+[void] $table.Columns.Add("Assigned Memory")
+[void] $table.columns.Add("Used Memory")
 
 
-        $table.Rows.Add($vmucpu)
+$vms = Get-VM | select-object -Property Name
 
+foreach ($vm in $vms) {
+    $vmdata = get-view -viewtype Virtual Machine -filter @{"Name"=$vm. Name}
+    $vmucpu = $vmdata.Summary.Quickstats.OverallCpuusage
+    if ($vmucpu -lt 200 -and $vmucpu -ne 0) {
 
+        $vmacpu = $vmdata.Summary.Runtime.MaxCpuusage
+        $vmumem = $vmdata.Summary.Quickstats.GuestMemoryusage
+        $vmamem = $vmdata.Summary.Runtime.MaxMemoryusage
+
+        [void] $table.Rows.Add($vm. name, $vmacpu, $vmucpu, $vmamem, $vmumem)
     }
 }
-    
-$table | export-csv C:\DataTableJobs.csv 
 
-    #$vmdata = get-view –viewtype VirtualMachine –filter @{“Name”=$vm.name}
-
-    #$vmcpu = $vmdata.Summary.QuickStats
-
-    #$vmcpuassigned = $vmdata.Summary
-
-
-    #
-    
-
+$table | export-csv C:\Idlevms.csv
